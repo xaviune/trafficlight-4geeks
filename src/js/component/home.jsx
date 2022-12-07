97cfbd1abe6638792db6b33baa023fd5bea09e8d
xaviune <xaviune@gmail.com>
2022-12-07 19:59:11 +0000

    ajustes esteticos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	const [selectedColor, setSelectedColor] = useState("");
 
 	return (
+		<>
+		<div className="Traffic-light-centered">
+		<div className="hanger"></div>
 		<div className="Traffic-light">
-			<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>	
+			<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
 			<div onClick={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
-			<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>		
+			<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
 		</div>
+		</div>
+		</>
 	);
 };
 
