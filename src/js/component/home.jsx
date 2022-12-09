7d424f150c09d451172ad18244b41e2a6f12cbbe
xaviune <xaviune@gmail.com>
2022-12-09 21:05:17 +0000

    añadidos los botones extras

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
+import Button from "react-bootstrap/Button"
 
 //create your first component
 const Home = () => {
 	const [selectedColor, setSelectedColor] = useState("");
+	const [activepurple, setActivePurple] = useState("");
 
 	return (
 		<>
 		<div className="Traffic-light-centered">
-		<div className="hanger"></div>
-		<div className="Traffic-light">
-			<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
-			<div onClick={() => setSelectedColor("yellow")} className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
-			<div onClick={() => setSelectedColor("green")} className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
-		</div>
+			<div className="hanger"></div>
+			<div className="Traffic-light">
+				<div onClick={() => setSelectedColor("1")} className={"light red" + (selectedColor === "1" ? " glow" : "")}></div>
+				<div onClick={() => setSelectedColor("2")} className={"light yellow" + (selectedColor === "2" ? " glow" : "")}></div>
+				<div onClick={() => setSelectedColor("3")} className={"light green" + (selectedColor === "3" ? " glow" : "")}></div>
+				{(activepurple == "on") ? <div onClick={() => setSelectedColor("4")} className={"light purple" + (selectedColor === "4" ? " glow" : "")}></div> : null }
+			</div>
+			<div className="d-grid gap-2 mt-3">
+      			<Button onClick={() => {(selectedColor == "1") ? setSelectedColor("2") : (selectedColor == "2") ? setSelectedColor("3") : (selectedColor == "3") ? setSelectedColor("4") : (selectedColor == "4") ? setSelectedColor("1") : setSelectedColor("1")} }
+				variant="primary" size="lg">
+      			  Change color
+      			</Button>
+      			<Button onClick={() => (activepurple == "on") ? setActivePurple("off") : setActivePurple("on")}
+				className="purple" variant="success" size="lg">
+      			  Add/delete purple
+      			</Button>
+    		</div>
 		</div>
 		</>
 	);
